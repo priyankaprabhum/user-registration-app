72b8bc59e2e0c4726d3c926142c7c5d71cdfe5a4
Rhishi Nema <rhishi.nema@ge.com>
2017-02-11 16:23:36 -0500

    Added pom file and UserRegistrationConfig.

---
+++ src/main/java/com/user/config/UserRegistrationConfig.java
@@ -0,0 +1,13 @@
+package com.user.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationConfig {
+
+	public static void main(String args[]) {
+		SpringApplication.run(UserRegistrationConfig.class, args);
+	}
+
+}
