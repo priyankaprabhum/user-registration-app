fd19b93901998766de79b10469905227923a1dbf
Rhishi Nema <rhishi.nema@ge.com>
2017-02-11 21:17:17 -0500

    Integrated User registration UI

--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,38 +24,61 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
-	public String hello(Model model,
-			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		model.addAttribute("userForm", new User());
-		model.addAttribute("name", name);
-		return "hello";
-	}
-
+	/**
+	 * GET registration form
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/registration", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
 	public String registration(Model model) {
 		model.addAttribute("userForm", new User());
 		return "registration";
 	}
 
+	/**
+	 * Create user form
+	 * 
+	 * @param userForm
+	 * @return
+	 */
 	@RequestMapping(value = "/registration", method = RequestMethod.POST, produces = MediaType.TEXT_HTML_VALUE)
-	public String registerUser(@ModelAttribute("user") User userForm) {
+	public ModelAndView registerUser(@ModelAttribute("user") User userForm) {
 		System.out.println(userForm);
-		return "success";
+		userService.createNewUser(userForm);
+		return new ModelAndView("redirect:/user/" + userForm.getUserName());
 	}
 
-	@RequestMapping(value = "{userId}", method = RequestMethod.GET)
+	/**
+	 * Get user by id
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	@RequestMapping(value = "/id/{userId}", method = RequestMethod.GET)
 	public ModelAndView getUserInfo(@PathVariable("userId") long userId) {
 		User user = userService.getUserInformation(userId);
 		return new ModelAndView("user", "user", user);
 	}
 
-	@RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public @ResponseBody void createUserInfo(@RequestBody User user) {
+	/**
+	 * Get user by user name
+	 * 
+	 * @param userName
+	 * @return
+	 */
 
+	@RequestMapping(value = "/{userName}", method = RequestMethod.GET)
+	public ModelAndView getUserByUserName(@PathVariable("userName") String userName) {
+		User user = userService.getUserByUsername(userName);
+		return new ModelAndView("user", "user", user);
+	}
+
+	@RequestMapping(value = "/json/createuser", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public @ResponseBody void createUserInfo(@RequestBody User user) {
 		userService.createNewUser(user);
 	}
-	
+
 	@RequestMapping(value = "/json/{userName}", method = RequestMethod.GET)
 	public @ResponseBody User getUserInfoByUserName(@PathVariable("userName") String userName) {
 		User user = userService.getUserByUsername(userName);
