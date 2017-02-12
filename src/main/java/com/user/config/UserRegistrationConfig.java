02c59455347d36afd70909f819e3959558bc0b5e
Rhishi Nema <rhishi.nema@ge.com>
2017-02-11 19:10:21 -0500

    GET user html

--- src/main/java/com/user/config/UserRegistrationConfig.java
+++ src/main/java/com/user/config/UserRegistrationConfig.java
@@ -1,10 +1,10 @@
 package com.user.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan({ "com.user.*"})
 public class UserRegistrationConfig {
 
--- src/main/java/com/user/controller/UserController.java
+++ src/main/java/com/user/controller/UserController.java
@@ -2,27 +2,49 @@ package com.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.user.model.User;
 import com.user.services.UserService;
 
-@RestController
-@RequestMapping(value = "/user")
+@Controller
+@RequestMapping(value = "user")
 public class UserController {
 
 	@Autowired
-	public UserService userService;
-	
-	@RequestMapping(value = "{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public @ResponseBody User getUserInfo(@PathVariable("userId")long userId){
-		
-		User user = userService.getUserInformation(userId);
-		return user;
+	private UserService userService;
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public String hello(Model model,
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		model.addAttribute("userForm", new User());
+		model.addAttribute("name", name);
+		return "hello";
 	}
-	
+
+	@RequestMapping(value = "/registration", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public String registration(Model model) {
+		model.addAttribute("userForm", new User());
+		return "registration";
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+		model.addAttribute("userForm", new User());
+		return "registration";
+	}
+
+	@RequestMapping(value = "{userId}", method = RequestMethod.GET)
+	public ModelAndView getUserInfo(@PathVariable("userId") long userId) {
+		User user = userService.getUserInformation(userId);
+		return new ModelAndView("user","user",user);
+	}
 }
